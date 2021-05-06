bd3289c4bccbc424ba1d92652bfb988942e5ab9e
therobgeorge <georg1rd@gmail.com>
2021-05-05 21:49:44 -0400

    completes bonus

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-actor = Actor.new(first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock")
-actor.save
+
+9.times do
+  Actor.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: FFaker::Movie.title)
+end
 
