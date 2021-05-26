87e608f0b5160ffeb31078fea48d15880cb1a566
therobgeorge <georg1rd@gmail.com>
2021-05-26 10:32:50 -0400

    adds movie_genre table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,4 +7,6 @@ class Movie < ApplicationRecord
   validates :plot, length: {in: 2..500}
   validates :director, length: {minimum: 2}
   validates :english, presence: true
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,13 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 
-Movie.create(title: "Plus One", year: 2019, plot: "Two college friends decide to be each other's plus one to every wedding of the summer. They fall in love. Per usual the dude messes the whole thing up. Alice is one of the best characters in movie history.")
+# Movie.create(title: "Plus One", year: 2019, plot: "Two college friends decide to be each other's plus one to every wedding of the summer. They fall in love. Per usual the dude messes the whole thing up. Alice is one of the best characters in movie history.")
 
-Movie.create(title: "Brick", year: 2006, plot: "A kid tries to find his girl friend and gets wrapped up in some banana town drug dealer stuff.")
+# Movie.create(title: "Brick", year: 2006, plot: "A kid tries to find his girl friend and gets wrapped up in some banana town drug dealer stuff.")
+
+MovieGenre.create!(genre_id: 1, movie_id: 1)
+MovieGenre.create!(genre_id: 2, movie_id: 2)
+MovieGenre.create!(genre_id: 1, movie_id: 3)
+MovieGenre.create!(genre_id: 4, movie_id: 4)
 
 
